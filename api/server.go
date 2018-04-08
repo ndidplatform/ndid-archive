906e1c23e66d1276212aecfdc09651e42dd2cae3
AnuchitO <anuchitp@thoughtworks.com>
2018-04-08 17:36:15 +0700

    Add flag tendermint port

--- api/server.go
+++ api/server.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
+	"github.com/ndidplatform/ndid/api/client/tendermint"
 	"github.com/ndidplatform/ndid/api/identity"
 	"github.com/ndidplatform/ndid/api/rp"
 )
@@ -13,6 +14,7 @@ var port string
 
 func initFlag() {
 	flag.StringVar(&port, "port", ":8000", "port for start service example :8000 or 127.0.0.1:8000")
+	flag.StringVar(&tendermint.TendermintAddr, "tenderm", "127.0.0.1:46657", "tendermint port example 127.0.0.1:46657 (please add ip address and port)")
 
 	flag.Parse()
 }
