840a3195bb5ae6d372f81187a5fb0d36f920c0b6
AnuchitO <anuchitp@thoughtworks.com>
2018-04-08 17:30:05 +0700

    Add flag port of api

--- api/server.go
+++ api/server.go
@@ -1,13 +1,24 @@
 package main
 
 import (
+	"flag"
+
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
 	"github.com/ndidplatform/ndid/api/identity"
 	"github.com/ndidplatform/ndid/api/rp"
 )
 
+var port string
+
+func initFlag() {
+	flag.StringVar(&port, "port", ":8000", "port for start service example :8000 or 127.0.0.1:8000")
+
+	flag.Parse()
+}
+
 func main() {
+	initFlag()
 	e := echo.New()
 
 	e.Use(middleware.Logger())
@@ -18,5 +29,5 @@ func main() {
 
 	e.POST("/rp/requests/:ns/:id", rp.CreateRequest)
 
-	e.Logger.Fatal(e.Start(":8000"))
+	e.Logger.Fatal(e.Start(port))
 }
